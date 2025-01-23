05a4827e31471abc36f130e6d95083bb5d53569a
Prince <princeagyeitwum0@gmail.com>
2025-01-23 14:59:36 +0000

    Implemented google authentiction

---
+++ app/auth/google/page.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+function page() {
+  return <button>ddsdg</button>;
+}
+
+export default page;
---
+++ utils/continue-with-googleauth.ts
@@ -0,0 +1,30 @@
+import { toast } from "react-toastify";
+
+export async function continueWithGoogleAuth() {
+  try {
+    const url = `${
+      process.env.NEXT_PUBLIC_HOST
+    }/api/o/google-oauth2/?redirect_uri=${
+      process.env.NODE_ENV == "production"
+        ? process.env.NEXT_PUBLIC_REDIRECT_URL
+        : "http://localhost:3000/auth/google"
+    }`;
+
+    const res = await fetch(url, {
+      method: "GET",
+      headers: {
+        Accept: "application/json",
+      },
+    });
+
+    const data = await res.json();
+
+    if (res.status == 200 && typeof window != "undefined") {
+      window.location.replace(data.authorization_url);
+    } else {
+      toast.error("something went wrong");
+    }
+  } catch {
+    toast.error("something went wrong");
+  }
+}
